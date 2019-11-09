dec0491969ef4db72cf87bc68f2ddb2fc6696782
André S. Hansen <andre.hansen@dyrket.no>
2019-11-09 17:15:13 +0100

    WorkdayCalendar.SetHoliday #1

--- WorkdayCalendar.Tests/WorkdayCalendarTests.cs
+++ WorkdayCalendar.Tests/WorkdayCalendarTests.cs
@@ -1,14 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace WorkdayCalendar.Tests
 {
+    /*
+     * Jeg liker å navngi klasser etter følgende format: [TypeTest]_[Klasse]_[Metode]_[ExtraInfo]
+     * [TypeTest] er enten UT for UnitTest eller IT for Integrasjonstest.
+     * [Klasse] er navnet på klassen som testes
+     * [Metode] er navnet på klassens metode som testes
+     * [ExtraInfo] er annet verdt å ha i navnet, for eks hvis metoden som testes er kompleks og kan ha forskjellig input
+     * 
+     * Intensjonen er å enkelt i alle perspektiver kunne detektere hva som har feilet, om det er i visual studio test eksploreren
+     * eller i logger fra eksterne systemer som kjører tester automatisk. Det gjør det også enklere å søke etter tester i test explorer.
+     * 
+     * I større komplekse prosjekter kan jeg legge [Namespace] eller [Område] før [Klasse] som en ekstra "path"-identifikator.
+     */
     [TestClass]
     public class WorkdayCalendarTests
     {
         [TestMethod]
-        public void UT_WorkdayCalendar_Set()
+        public void UT_WorkdayCalendar_SetHoliday()
         {
             WorkdayCalendar workdayCalendar = new WorkdayCalendar();
+
+            workdayCalendar.SetHoliday(new DateTime(2004, 1, 1));
+            Assert.AreEqual(1, workdayCalendar.GetHolidays().Count);
+            Assert.AreEqual(workdayCalendar.GetHolidays()[0], new DateTime(2004, 1, 1));
+
+            workdayCalendar.SetHoliday(new DateTime(2004, 1, 2));
+            Assert.AreEqual(2, workdayCalendar.GetHolidays().Count);
         }
     }
 }
--- WorkdayCalendar/WorkdayCalendar.cs
+++ WorkdayCalendar/WorkdayCalendar.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WorkdayCalendar
 {
     public class WorkdayCalendar : IWorkdayCalendar
     {
+        private List<DateTime> Holidays = new List<DateTime>();
+
+
+        /// <summary>
+        /// Add this date to the list of holidays. Only the .Date component is considered.
+        /// </summary>
         public void SetHoliday(DateTime date)
         {
-            throw new NotImplementedException();
+            /* 
+             * - Henter bare ut .Date komponenten av DateTime, som en ekstra sikring for å sørge for at man ikke senere i 
+             * koden får følgefeil.
+             * 
+             * - Man kan argumentere for at man skal kaste exceptions hvis date input har timer, sekunder eller ms satt,
+             * men dette mener jeg er for agressive kasting av exceptions da en kan tenke seg at en bruker av denne klassen
+             * med uhell sender inn denne infoen i sære tilfeller. I tillegg så er det intuitivt å forstå at denne metoden omhandler
+             * hele dager når det ikke er en til-fra metode, og er også dokumenter i metode-summary.
+             * 
+             */
+            Holidays.Add(date.Date);
         }
 
         public void SetRecurringHoliday(int month, int day)
@@ -17,6 +34,11 @@ namespace WorkdayCalendar
         public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
         {
             throw new NotImplementedException();
+        }
+
+        public List<DateTime> GetHolidays()
+        {
+            return Holidays;
         }
 
         public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
