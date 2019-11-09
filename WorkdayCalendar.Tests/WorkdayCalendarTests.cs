e66edc107c0cbb0378fb7c7372ca3e5ec0fadd5c
André S. Hansen <andre.hansen@dyrket.no>
2019-11-09 20:54:57 +0100

    Can get workday after incremented workdays number with weekday, holidays and within workday consideration #1 #2 #3 #4

--- WorkdayCalendar.Tests/WorkdayCalendarTests.cs
+++ WorkdayCalendar.Tests/WorkdayCalendarTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Globalization;
 
 namespace WorkdayCalendar.Tests
 {
@@ -18,17 +19,84 @@ namespace WorkdayCalendar.Tests
     [TestClass]
     public class WorkdayCalendarTests
     {
-        [TestMethod]
-        public void UT_WorkdayCalendar_SetHoliday()
+
+        private const string DATE_FORMAT = "dd-MM-yyyy HH:mm";
+
+        /*
+         * Jeg kunne initialisert WorkdayCalendar i TestInitialize. Jeg liker ikke å gjøre dette, for å begrense "magi"
+         * som skjer utenfor hver enkelt testmetode. Jeg liker å raskt kunne "se" alt som skjer innad hver enkelt testmetode.
+         * 
+         * Man kan argumentere for at alle som leser tester bør ha et forhold til TestInitialize eller tilsvarende, men her heller jeg mot,
+         * og istedet repetere kode. Hadde opprettelsen av WorkdayCalendar vær kompleks hadde jeg lagt den i en egen opprettelses-hjelpemetode.
+         */
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [DataTestMethod]
+        [DataRow(00, 00, 00, 00,   0)]
+        [DataRow(00, 00, 01, 00,   60)]
+        [DataRow(00, 00, 01, 01,   61)]
+        [DataRow(23, 00, 00, 01,   61)]
+        public void UT_WorkdayCalendar_GetWorkdayDuration(int startHours, int startMinutes, int stopHours, int stopMinutes, int expTotalMinutes)
         {
             WorkdayCalendar workdayCalendar = new WorkdayCalendar();
+            workdayCalendar.SetWorkdayStartAndStop(startHours, startMinutes, stopHours, stopMinutes);
 
-            workdayCalendar.SetHoliday(new DateTime(2004, 1, 1));
-            Assert.AreEqual(1, workdayCalendar.GetHolidays().Count);
-            Assert.AreEqual(workdayCalendar.GetHolidays()[0], new DateTime(2004, 1, 1));
+            Assert.AreEqual(expTotalMinutes, workdayCalendar.GetWorkdayDuration().TotalMinutes);
+        }
 
-            workdayCalendar.SetHoliday(new DateTime(2004, 1, 2));
-            Assert.AreEqual(2, workdayCalendar.GetHolidays().Count);
+        [DataTestMethod]
+        // Starts and stops on same day
+        [DataRow(00, 00, 08, 00, "24-05-2004 00:00",   true)]
+        [DataRow(00, 00, 08, 00, "24-05-2004 00:01",   true)]
+        [DataRow(00, 00, 08, 00, "24-05-2004 07:59",   true)]
+        [DataRow(00, 00, 08, 00, "24-05-2004 08:00",   true)]
+        [DataRow(00, 00, 08, 00, "24-05-2004 08:01",   false)]
+        [DataRow(00, 00, 08, 00, "24-05-2004 23:59",   false)]
+        // Starts and stops crosses midnight
+        [DataRow(23, 00, 01, 00, "24-05-2004 23:00",   true)]
+        [DataRow(23, 00, 01, 00, "24-05-2004 23:01",   true)]
+        [DataRow(23, 00, 01, 00, "24-05-2004 00:59",   true)]
+        [DataRow(23, 00, 01, 00, "24-05-2004 01:00",   true)]
+        [DataRow(23, 00, 01, 00, "24-05-2004 01:01",   false)]
+        [DataRow(23, 00, 01, 00, "24-05-2004 22:59",   false)]
+        public void UT_WorkdayCalendar_IsWithinWorkday(int startHours, int startMinutes, int stopHours, int stopMinutes, string dateStr, bool expIsWithinWorkday)
+        {
+            WorkdayCalendar workdayCalendar = new WorkdayCalendar();
+            workdayCalendar.SetWorkdayStartAndStop(startHours, startMinutes, stopHours, stopMinutes);
+
+            DateTime date = DateTime.ParseExact(dateStr, DATE_FORMAT, CultureInfo.InvariantCulture);
+            Assert.AreEqual(expIsWithinWorkday, workdayCalendar.IsWithinWorkday(date));
+        }
+
+        [DataTestMethod]
+        // Forwards
+        [DataRow(00, 00, 08, 00, "24-05-2004 00:00", 1.0,   "24-05-2004 08:00")]
+        [DataRow(00, 00, 08, 00, "24-05-2004 00:00", 2.0,   "25-05-2004 08:00")]
+        // Backwards
+        [DataRow(00, 00, 08, 00, "24-05-2004 08:00", -1.0,   "24-05-2004 00:00")]
+        [DataRow(00, 00, 08, 00, "25-05-2004 08:00", -2.0,   "24-05-2004 00:00")]
+        public void UT_WorkdayCalendar_GetWorkdayAfterIncremetedWorkdays(
+            int startHours,
+            int startMinutes,
+            int stopHours,
+            int stopMinutes,
+            string startDateStr,
+            double incrementWorkDays, // DataRow attribute dont accept decimals
+            string expWorkdayStr
+        )
+        {
+            WorkdayCalendar workdayCalendar = new WorkdayCalendar();
+            workdayCalendar.SetWorkdayStartAndStop(startHours, startMinutes, stopHours, stopMinutes);
+
+            DateTime workday = workdayCalendar.GetWorkdayAfterIncremetedWorkdays(
+                DateTime.ParseExact(startDateStr, DATE_FORMAT, CultureInfo.InvariantCulture),
+                (decimal)incrementWorkDays
+            );
+
+            Assert.AreEqual(expWorkdayStr, workday.ToString(DATE_FORMAT));
         }
     }
 }
--- WorkdayCalendar/WorkdayCalendar.cs
+++ WorkdayCalendar/WorkdayCalendar.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿/*
+ * Du vil se at det er summary kommentarer på engelsk, dette er kommentarer til den teoretiske utvikleren som bruker denne klassen.
+ * 
+ * De norske kommentarene i metodene er mine kommentarer ifb Vivende code review.
+ */
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WorkdayCalendar
 {
     public class WorkdayCalendar : IWorkdayCalendar
     {
         private List<DateTime> Holidays = new List<DateTime>();
+        private List<(int Month, int Day)> RecurringHolidays = new List<(int Month, int Day)>();
 
+        private int StartHours;
+        private int StartMinutes;
+        private int StopHours;
+        private int StopMinutes;
+
+        private readonly List<DayOfWeek> WeekendDays = new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday };
 
         /// <summary>
         /// Add this date to the list of holidays. Only the .Date component is considered.
@@ -21,19 +34,42 @@ namespace WorkdayCalendar
              * men dette mener jeg er for agressive kasting av exceptions da en kan tenke seg at en bruker av denne klassen
              * med uhell sender inn denne infoen i sære tilfeller. I tillegg så er det intuitivt å forstå at denne metoden omhandler
              * hele dager når det ikke er en til-fra metode, og er også dokumenter i metode-summary.
-             * 
              */
             Holidays.Add(date.Date);
         }
 
         public void SetRecurringHoliday(int month, int day)
         {
-            throw new NotImplementedException();
+            RecurringHolidays.Add((Month: month, Day: day));
         }
 
         public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
         {
-            throw new NotImplementedException();
+            StartHours = startHours;
+            StartMinutes = startMinutes;
+            StopHours = stopHours;
+            StopMinutes = stopMinutes;
+        }
+
+        public TimeSpan GetWorkdayDuration()
+        {
+            if (IsWorkdayStartAndStopOnSameDay())
+                return new DateTime(2000, 1, 1, StopHours, StopMinutes, 0) - new DateTime(2000, 1, 1, StartHours, StartMinutes, 0);
+            else // Arbeidsdagen krysser natten
+                return new DateTime(2000, 1, 2, StopHours, StopMinutes, 0) - new DateTime(2000, 1, 1, StartHours, StartMinutes, 0);
+        }
+
+        public bool IsWithinWorkday(DateTime d)
+        {
+            if (IsWorkdayStartAndStopOnSameDay())
+                return IsBetweenStartAndStop(d);
+            else // Arbeidstid krysser natten
+            {
+                if (IsBetweenStartAndStop(d, -1, 0))
+                    return true;
+                else
+                    return IsBetweenStartAndStop(d, 0, 1);
+            }
         }
 
         public List<DateTime> GetHolidays()
@@ -41,14 +77,56 @@ namespace WorkdayCalendar
             return Holidays;
         }
 
-        public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
+        /// <summary>
+        /// Get the next workday after the startDate incremeted by given amount of workdays have lapsed. Precision to minutes.
+        /// </summary>
+        public DateTime GetWorkdayAfterIncremetedWorkdays(DateTime startDate, decimal incrementInWorkdays)
         {
-            throw new NotImplementedException();
+            /*
+             * - Konverterer TotalMinutes til decimal da det er en tryggere konversion
+             * - Runder til int da presisjonen til programmet begrenses til minutter
+             */
+            int totalWorkdayMinutes = (int)Math.Round(Convert.ToDecimal(GetWorkdayDuration().TotalMinutes) * incrementInWorkdays);
+            int elapsedWorkdayMinutes = 0;
+
+            // Simuler tiden bakover/fremover til vi har passert nok arbeidsminutter
+            int scanMinuteIncremention = incrementInWorkdays < 0 ? -1 : 1;
+            DateTime scanDate = new DateTime(startDate.Year, startDate.Month, startDate.Day, startDate.Hour, startDate.Minute, 0);
+
+            while (elapsedWorkdayMinutes != totalWorkdayMinutes)
+            {
+                scanDate = scanDate.AddMinutes(scanMinuteIncremention);
+
+                if (WeekendDays.Contains(scanDate.DayOfWeek))
+                    continue;
+                if (Holidays.Any(h => h.Date == scanDate.Date))
+                    continue;
+                if (RecurringHolidays.Any(h => h.Month == scanDate.Month && h.Day == scanDate.Day))
+                    continue;
+
+                /*
+                 * Siden vi for vær while loop egentlig vurderer en 1-minutts-periode, så må vi sjekk
+                 * både minuttet vi er på, og minuttet vi kommer fra. På den måten vurderer man at hele minuttet er innenfor.
+                 */ 
+                if (!IsWithinWorkday(scanDate) || !IsWithinWorkday(scanDate.AddMinutes(-scanMinuteIncremention)))
+                    continue;
+
+                elapsedWorkdayMinutes += scanMinuteIncremention;
+            }
+
+            return scanDate;
         }
 
-        public string GetName()
+        private bool IsWorkdayStartAndStopOnSameDay()
         {
-            throw new NotImplementedException();
+            return StartHours < StopHours || (StartHours == StopHours && StartMinutes <= StopMinutes);
+        }
+
+        private bool IsBetweenStartAndStop(DateTime d, int startDelta = 0, int stopDelta = 0)
+        {
+            DateTime start = new DateTime(d.Year, d.Month, d.Day, StartHours, StartMinutes, 0).AddDays(startDelta);
+            DateTime end = new DateTime(d.Year, d.Month, d.Day, StopHours, StopMinutes, 0).AddDays(stopDelta);
+            return start <= d && d <= end;
         }
     }
 }
