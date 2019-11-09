d3ee11588a50628a93aa6d135962587cb02e0540
André S. Hansen <andre.hansen@dyrket.no>
2019-11-09 16:52:58 +0100

    Initial project structure and documentation

---
+++ WorkdayCalendar.Tests/WorkdayCalendarTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkdayCalendar.Tests
+{
+    [TestClass]
+    public class WorkdayCalendarTests
+    {
+        [TestMethod]
+        public void UT_WorkdayCalendar_Set()
+        {
+            WorkdayCalendar workdayCalendar = new WorkdayCalendar();
+        }
+    }
+}
---
+++ WorkdayCalendar/IWorkdayCalendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkdayCalendar
+{
+    public interface IWorkdayCalendar
+    {
+        void SetHoliday(DateTime date);
+        void SetRecurringHoliday(int month, int day);
+        void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes);
+
+        DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays);
+        string GetName();
+    }
+}
